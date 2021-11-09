89615fa4741c551a9901fea54f35ad27dd45c0f2
gamegame2 <59419591+dks2312@users.noreply.github.com>
2021-11-09 22:45:25 +0900

    Object Pooling End

---
+++ Skill1-Object Pooling/Assets/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    Rigidbody rigidbody = null;
+
+    private void OnEnable() {
+        if(rigidbody == null){
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        rigidbody.AddExplosionForce(1000, transform.position, 1f);
+        StartCoroutine(DestroyCube());
+    }
+
+    IEnumerator DestroyCube(){
+        yield return new WaitForSeconds(1f);
+        ObjectPoolingManager.instance.InsertQueue(gameObject);
+    }
+}
