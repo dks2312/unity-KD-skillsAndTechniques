61fefb396965adb1b0e74598ef12b9416c96768c
gamegame2 <59419591+dks2312@users.noreply.github.com>
2021-11-11 09:45:35 +0900

    스킬별 오브젝트&스크립트 정리

---
+++ skills/Assets/Skill1-ObjectPooling/CreateCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(CreateCorouttine());
+    }
+
+    IEnumerator CreateCorouttine(){
+        while(true){
+            yield return null;
+            GameObject t_object = ObjectPoolingManager.instance.GetQueue();
+            t_object.transform.position = transform.position;
+        }
+    }
+}
