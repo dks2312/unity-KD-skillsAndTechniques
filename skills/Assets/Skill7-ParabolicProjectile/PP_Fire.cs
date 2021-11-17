26b09af048bd761f8e470add248f2c5858eb24ee
gamegame2 <59419591+dks2312@users.noreply.github.com>
2021-11-17 22:04:55 +0900

    project file organize

--- skills/Assets/Skill7-ParabolicProjectile/PP_Fire.cs
+++ skills/Assets/Skill7-ParabolicProjectile/PP_Fire.cs
@@ -16,9 +16,12 @@ public class PP_Fire : MonoBehaviour
 
     void LookAtMouse(){
         Vector3 t_mousePos = m_cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 t_direction = new Vector3(t_mousePos.x - m_tfArrow.position.x, t_mousePos.y - m_tfArrow.position.y, m_tfArrow.position.z);
+        
+        Vector3 t_direction = new Vector3(t_mousePos.x, t_mousePos.y, m_tfArrow.position.z);
+        // Vector2 t_direction = new Vector2(t_mousePos.x - m_tfArrow.position.x, t_mousePos.y - m_tfArrow.position.y);
 
-        m_tfArrow.right = t_direction;
+        m_tfArrow.LookAt(t_direction);
+        
     }
 
     void TryFire(){
@@ -31,7 +34,8 @@ public class PP_Fire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LookAtMouse();
+        //현재 씬에서 할려면 설정할 것들이 생기고 그로 인해 전체적으로 수정이 필요해짐 = 다음 기회에
+        // LookAtMouse(); 
         TryFire();
     }
 }
