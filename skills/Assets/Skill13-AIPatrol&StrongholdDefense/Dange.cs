2c238e0b5240f4c2ba4de4ad3bd587457162a429
gamegame2 <59419591+dks2312@users.noreply.github.com>
2021-11-21 01:40:44 +0900

    Skill13-AIPatrol&BaseDefense end

---
+++ skills/Assets/Skill13-AIPatrol&StrongholdDefense/Dange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dange : MonoBehaviour
+{
+    [SerializeField] EuemyAI m_enemy = null;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")){
+            m_enemy.SetTarget(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Player")){
+            m_enemy.RemoveTarget();
+        }
+    }
+}
